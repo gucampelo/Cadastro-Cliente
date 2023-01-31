bb7fd4f5f39a69831171c5eade00f4e468e4c5f5
Gustavo Campelo <gustavo.santana12@fatec.sp.gov.br>
2023-01-30 23:47:34 -0300

    teste novo

--- controller/routes/teste.js
+++ controller/routes/teste.js
@@ -1,6 +1,6 @@
 //feito na mão
 var http = require('http');
 
-http.createServer().listen(8081);
+http.createServer().listen(8084);
 
 console.log("O servidor está funcionando!");
---
+++ controller/routes/teste2.js
@@ -0,0 +1,13 @@
+// Feito agora com o express
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, resp){
+    resp.send("Bem-vindo ao meu app");
+})
+
+//esta deve ser sempre a última linha quando usamos o express
+app.listen(8084, function(){
+    console.log("Servidor funcionando na url http://localhost:8084");
+});

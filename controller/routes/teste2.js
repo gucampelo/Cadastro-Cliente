aa2e791d32a8cdc2236b45633a1ed922c15a7f5f
Gustavo Campelo <gustavo.santana12@fatec.sp.gov.br>
2023-02-02 22:31:29 -0300

    teste 2 2

--- controller/routes/teste2.js
+++ controller/routes/teste2.js
@@ -8,7 +8,20 @@ app.get("/", function(req, resp){
     resp.send("Bem-vindo ao meu app");
 })
 
+app.get("/contato", function(req, resp){
+    resp.send("Página de contato do meu app");
+})
+
+app.get("/produto", function(req, resp){
+    resp.send("Página de produto do meu app");
+})
+
+app.get("/dados/:nome/:cargo", function(req, resp){
+    resp.send("<h1>Ola sr(a). "+req.params.nome+"</h1><h2>Seu cargo é "+req.params.cargo+"</h2>");
+})
+
 //esta deve ser sempre a última linha quando usamos o express
+//o listen fica ouvindo todas as conexões no servidor, localhost é a minha máquina
 app.listen(8084, function(){
     console.log("Servidor funcionando na url http://localhost:8084");
 });
